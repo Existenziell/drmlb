26b611725bdabc00cac2725fa5248a855f5f25a0
Christof <bauer.christof@gmail.com>
2022-10-19 23:03:59 -0500

    Typo

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const e1ExclusiveNftAddress = '0x5cA07072dBCE79581f3138Ab3fF4bFa7d316a70F'
-export const e1NftAddress = '0x17F0d103F053634B428189F900fc3d3184c7a5Cd'
-export const e1TokenAddress = '0x4CCE7ac0B91167a0716ADA2e2830A5974Ec72b21'
-
-export const e1contractAddress = '0x4CCE7ac0B91167a0716ADA2e2830A5974Ec72b21'
+export const e1ExclusiveNftAddress = '0x3866f158aCE387C417b7712a363f270Aff989c87'
+export const e1NftAddress = '0xf2809ecA7475A9f7871797e379F6b902E335a286'
+export const e1TokenAddress = '0x5a9665483e408461a63FC37F11D36E29b80E85D0'
+  
+export const e1contractAddress = '0x3866f158aCE387C417b7712a363f270Aff989c87'
